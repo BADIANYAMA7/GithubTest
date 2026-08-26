96ec17eba34dc41a5d0c532f6c166f4bd9c0680e
hp <ben.blessing2006@gmail.com>
2026-08-26 13:47:33 +0200

    Added Patients Attributes(Name,LastName,Age...etc) Arrays

--- src/st10497775_practical_assignment1/ST10497775_Practical_Assignment1.java
+++ src/st10497775_practical_assignment1/ST10497775_Practical_Assignment1.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package st10497775_practical_assignment1;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -11,9 +13,14 @@ import java.util.Scanner;
  */
 public class ST10497775_Practical_Assignment1 {
 
-    
+    public static ArrayList<String> PatientName = new ArrayList<>();
+    public static ArrayList<String> LastName = new ArrayList<>();
+    public static ArrayList<Integer> Age = new ArrayList<>();
+    public static ArrayList<String> Gender = new ArrayList<>();
+    public static ArrayList<String> Medical = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Welcome To Medicare Hospital");
     }
-    
+
 }
