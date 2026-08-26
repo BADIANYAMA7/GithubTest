cb42dd5d8c6460254e40e7d8f5804c9f68b14144
hp <ben.blessing2006@gmail.com>
2026-08-26 13:09:33 +0200

    Start of Practical Assignment

---
+++ src/st10497775_practical_assignment1/ST10497775_Practical_Assignment1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package st10497775_practical_assignment1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author hp
+ */
+public class ST10497775_Practical_Assignment1 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome To Medicare Hospital");
+    }
+    
+}
